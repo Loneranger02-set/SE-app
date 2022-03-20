641eb626b781069ec0684067bb259ee4cdf6b27e
Akshay Mammen Koshy <akshaykoshy2000@gmail.com>
2022-03-20 21:36:11 +0530

    Splashscreen layout + Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
 import 'loginScreen.dart';
 import 'menuPage.dart';
+import 'dart:async';
 void main() {
   runApp(MyApp());
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                LoginScreen()
+            )
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text('VISION',textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 20,
+              color:Colors.black
+          )),
+        decoration: BoxDecoration(
+
+          color: Colors.white,
+
+        image: DecorationImage(
+        image: AssetImage(
+        'assets/images/download.jpg'),
+
+    ),
+    ),);
+  }
 }
 class MyApp extends StatelessWidget {
   @override
@@ -10,7 +50,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title:'Flutter login UI',
       debugShowCheckedModeBanner: false,
-      home:LoginScreen(),
+      home:MyHomePage(),
     );
   }
 }
