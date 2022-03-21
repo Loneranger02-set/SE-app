1aab8e1d00223acfc3410689c9e56d1c7854bd1a
Loneranger02-set <anishas.cs19@rvce.edu.in>
2022-03-21 05:37:17 +0530

    Color gradient for text

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'loginScreen.dart';
 import 'menuPage.dart';
 import 'dart:async';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -14,7 +16,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3),
+    Timer(Duration(seconds: 20),
             ()=>Navigator.pushReplacement(context,
             MaterialPageRoute(builder:
                 (context) =>
@@ -31,14 +33,18 @@ class _MyHomePageState extends State<MyHomePage> {
         //crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           SizedBox(height: 475,),
-          Text('VISION',//textAlign: TextAlign.center,
+          GradientText('VISION',//textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 20,
-                color:Colors.black,
                 fontWeight: FontWeight.bold,
-                decoration: TextDecoration.none
-                //height: 100,
-              )
+                decoration: TextDecoration.none,
+              ),
+            colors: [
+              Colors.deepPurple,
+              Colors.greenAccent,
+              Colors.pinkAccent,
+              Colors.teal,
+            ],
           ),
         ],
       ),
